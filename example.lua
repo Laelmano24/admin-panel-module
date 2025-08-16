d62effb92e0fcbfa7040845ae4cb8035f161c3fd
Rael_Dev <162019823+Laelmano24@users.noreply.github.com>
2025-08-16 16:26:20 -0300

    Add files via upload

---
+++ example.lua
@@ -0,0 +1,33 @@
+local adminModules = loadstring(game:HttpGet("https://raw.githubusercontent.com/hypertext500/admin-panel-module/refs/heads/main/loader.lua"))()
+
+--[[
+
+    Comandos de admin:
+
+    /revelar [nome do jogador]
+    /revelarAll
+
+    /kill [nome do jogador]
+    /killAll
+
+    /bring [nome do jogador]
+    /bringAll
+
+    /kick [nome do jogador]
+    /kickAll
+
+    /dizerExecutor [nome do jogador]
+    /dizerExecutorAll
+
+    /dizer [palavra ou frase]
+
+]]
+
+adminModules.Init({
+
+    prefix = "/",
+    whitelist = {"RaelAdminScripter"},
+    kickMessage = "Você foi chutado por um admin",
+    revelarMessage = "EU ESTOU USANDO MEU SCRIPT"
+
+})
